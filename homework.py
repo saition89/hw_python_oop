df2e1de12a9d02ae92feed332725c78a21eebc33
Vladimir <saition89@yandex.ru>
2021-11-30 19:33:45 +0300

    changed inputing date format

--- homework.py
+++ homework.py
@@ -1,5 +1,6 @@
 import datetime as dt
 
+DATE_FORMAT = '%d.%m.%Y'
 
 class Calculator:
     def __init__(self, balance):
@@ -9,8 +10,6 @@ class Calculator:
 
     def add_record(self, record):
         self.records.append(record)
-        if record.date == (dt.datetime.now()).date():
-            self.balance -= record.amount
 
     def get_week_stats(self):
         current_week = []
@@ -25,11 +24,12 @@ class Calculator:
 
 class Record:
     def __init__(self, amount, comment, date=None):
-        if date is None:
-            date = (dt.datetime.now()).date()
         self.amount = amount
+        if date is None:
+            self.date = dt.date.today()
+        else:
+            self.date = dt.datetime.strptime(date, DATE_FORMAT).date()
         self.comment = comment
-        self.date = date
 
 
 class CashCalculator(Calculator):
@@ -37,42 +37,50 @@ class CashCalculator(Calculator):
     def get_today_stats(self):
         spent = 0
         for i in self.records:
-            if i.date == (dt.datetime.now()).date():
+            if i.date == dt.date.today():
                 spent += i.amount
         return f"Сегодня потрачено {spent} руб"
 
     def get_today_cash_remained(self, currency):
-        if self.balance > 0:
+        spent=0
+        for i in self.records:
+            if i.date == dt.date.today():
+                spent += i.amount
+        if self.balance-spent > 0:
             if currency == 'rub':
-                return f"На сегодня осталось {self.balance} рублей"
+                return f"На сегодня осталось {self.balance-spent} рублей"
             if currency == 'eur':
-                return f"На сегодня осталось {self.balance / 84} евро"
+                return f"На сегодня осталось {(self.balance-spent) / 84} евро"
             if currency == 'usd':
-                return f"На сегодня осталось {self.balance / 74} долларов"
+                return f"На сегодня осталось {(self.balance-spent) / 74} долларов"
 
-        if self.balance == 0:
+        if self.balance-spent == 0:
             return "Денег нет, держись"
 
-        if self.balance < 0:
+        if self.balance-spent < 0:
             if currency == 'rub':
-                return f"Денег нет, держись: твой долг {self.balance} рублей"
+                return f"Денег нет, держись: твой долг {self.balance-spent} рублей"
             if currency == 'eur':
-                return f"Денег нет, держись: твой долг {self.balance / 84} евро"
+                return f"Денег нет, держись: твой долг {(self.balance-spent) / 84} евро"
             if currency == 'usd':
-                return f"Денег нет, держись: твой долг {self.balance / 74} долларов"
+                return f"Денег нет, держись: твой долг {(self.balance-spent) / 74} долларов"
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        if self.balance > 0:
-            return f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {self.balance} кКал"
-        if self.balance <= 0:
+        got = 0
+        for i in self.records:
+            if i.date == dt.date.today():
+                got += i.amount
+        if self.balance-got > 0:
+            return f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {self.balance-got} кКал"
+        if self.balance-got <= 0:
             return "Хватит есть!"
 
     def get_today_stats(self):
         spent = 0
         for i in self.records:
-            if i.date == (dt.datetime.now()).date():
+            if i.date == dt.date.today():
                 spent += i.amount
         return f"Сегодня съедено {spent} ККал"
 
